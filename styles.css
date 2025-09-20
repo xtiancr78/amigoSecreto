fab9bc26baa19ee1b49ed4302c63cd69bacb92c4
Christian_Caballero <94983984+xtiancr78@users.noreply.github.com>
2025-09-20 12:54:14 -0500

    Crear styles.css

---
+++ styles.css
@@ -0,0 +1,22 @@
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+const listaAmigos = [];
+
+function agregarAmigo(){
+  const nombre = document.getElementById("amigo");
+  const lista = nombre.value.trim();
+  if(nombre &&! listaAmigos.includes(lista)){
+    listaAmigos.push(lista);
+    nombre.value="";
+    console.log(listaAmigos);
+  }
+}
+
+
+function sortearAmigo(){
+  const numeroAleatorio = Math.floor(Math.random() * listaAmigos.length);
+  console.log(numeroAleatorio);
+  console.log(listaAmigos[numeroAleatorio]);
+  
+  
+}
